9435ef10bab078da890ab028fd525a5372060853
Tim Knoll <timothy.knoll@gmail.com>
2018-08-13 03:05:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Dog
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def bark=(dog_sound)
+    @bark = dog_sound
+  end
+
+  def bark
+    @bark
+  end        
+
+end  
